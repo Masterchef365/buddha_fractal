7f7913a6fd65d82da4152d2243425c89b321ab59
Masterchef365 <duncan.freeman1@gmail.com>
2022-05-07 01:18:16 -0700

    Adjustable cutoff, count to grayscale division

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,15 @@ struct Opt {
     #[structopt(short, long, default_value = "1.0")]
     scale: f32,
 
+    /// Cutoff disc radius. Optimally 2.0, 
+    /// but default to 3.0 for presentation purposes
+    #[structopt(long, default_value = "3.0")]
+    disc: f32,
+
+    /// Divide all bin counts by this number for image output
+    #[structopt(short, long, default_value = "2")]
+    div: u16,
+
     /// Total iterations
     #[structopt(short, long, default_value = "10000000")]
     iters: usize,
@@ -45,14 +54,15 @@ struct Opt {
     steps: usize,
 }
 
-fn mandelbrot(x: f32, y: f32) -> impl Iterator<Item = (f32, f32)> {
+fn mandelbrot(x: f32, y: f32, disc: f32) -> impl Iterator<Item = (f32, f32)> {
     let (mut a, mut b) = (0., 0.);
+    let r = disc * disc;
     std::iter::from_fn(move || {
         let tmp = a * a - b * b + x;
         b = 2. * a * b + y;
         a = tmp;
 
-        if a * a + b * b > 2. * 2. {
+        if a * a + b * b > r {
             None
         } else {
             Some((a, b))
@@ -63,27 +73,34 @@ fn mandelbrot(x: f32, y: f32) -> impl Iterator<Item = (f32, f32)> {
 fn worker_thread(args: Opt, iters: usize) -> Image {
     let mut image_data = vec![0_u16; args.width * args.height];
 
+    // Image framing
     let scale = |x: f32| ((x / args.scale) + 1.) / 2.;
-
     let aspect = args.width as f32 / args.height as f32;
 
+    // Save all steps taken
     let mut steps = Vec::with_capacity(args.steps);
-    for (idx, (x, y)) in disc(2.).take(iters).enumerate() {
-        steps.clear();
-        steps.extend(mandelbrot(x, y).take(args.steps));
 
+    for (idx, (x, y)) in disc(args.disc).take(iters).enumerate() {
+        steps.clear();
+        steps.extend(mandelbrot(x, y, args.disc).take(args.steps));
+
+        // Print progress
         if idx % 100_000 == 0 {
             println!("{}/{} ({}%)", idx, iters, idx * 100 / iters);
         }
 
+        // If the function diverged...
         if steps.len() != args.steps {
             for (x, y) in steps.drain(..) {
+                // Find position in image
                 let x = scale((x - args.center_x) / aspect) * args.width as f32;
                 let y = scale(y - args.center_y) * args.height as f32;
 
+                // Bounds check
                 let bound_x = x >= 0. && x < args.width as f32;
                 let bound_y = y >= 0. && y < args.height as f32;
 
+                // Write to image
                 if bound_x && bound_y {
                     let idx = x as usize + y as usize * args.width;
                     image_data[idx] = image_data[idx].saturating_add(1);
@@ -128,7 +145,7 @@ fn main() -> Result<()> {
     // Determine coloring
     let out_image: Vec<u8> = out_image
         .into_iter()
-        .map(|i| (i >> 1).min(u8::MAX as u16) as u8)
+        .map(|i| (i / args.div).min(u8::MAX as u16) as u8)
         .collect();
 
     // Write results
@@ -140,6 +157,7 @@ fn main() -> Result<()> {
     )
 }
 
+/// Produce random points on the unit disc with the given radius
 fn disc(radius: f32) -> impl Iterator<Item = (f32, f32)> {
     let unif = Uniform::new(-radius, radius);
     unif.sample_iter(rand::thread_rng())
@@ -147,6 +165,7 @@ fn disc(radius: f32) -> impl Iterator<Item = (f32, f32)> {
         .filter(move |(x, y)| x * x + y * y < radius * radius)
 }
 
+/// Write a grayscale PNG at the given path
 fn write_png(path: &Path, data: &[u8], width: u32, height: u32) -> Result<()> {
     let file = File::create(path)?;
     let ref mut w = BufWriter::new(file);
@@ -156,7 +175,7 @@ fn write_png(path: &Path, data: &[u8], width: u32, height: u32) -> Result<()> {
     encoder.set_depth(png::BitDepth::Eight);
 
     let mut writer = encoder.write_header()?;
-    writer.write_image_data(&data)?; // Save
-                                     //
+    writer.write_image_data(&data)?;
+
     Ok(())
 }
